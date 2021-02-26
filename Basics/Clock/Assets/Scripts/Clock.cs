d99999ce510e86d197fb2724ba38eaea075fd211
Mason Schneider <masondschneider@gmail.com>
2021-02-25 23:28:11 -0800

    Basics 1 -3
    This is the first three projects of Catlike tutorials in the Basics section.

---
+++ Basics/Clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegrees = -30f;
+    const float minutesToDegrees = -6f;
+    const float secondsToDegrees = -6f;
+
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    private void Update()
+    {
+        var time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+
+}
